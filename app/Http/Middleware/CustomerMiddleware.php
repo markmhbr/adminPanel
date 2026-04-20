b62e3a27ed455dbbd0cc955fedf3002d369a03bd
Markmhbr <markmhbr@example.com>
2026-04-20 21:58:07 +0700

    Perbaikan Middleware

--- app/Http/Middleware/CustomerMiddleware.php
+++ app/Http/Middleware/CustomerMiddleware.php
@@ -16,7 +16,7 @@ class CustomerMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::check() && (Auth::user()->role !== 'admin')) {
+        if (Auth::check() && in_array(Auth::user()->role, ['admin', 'customer'])) {
             return $next($request);
         }
 
